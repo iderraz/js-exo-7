e907bd00353ddfcc0287364c42b7e308231d71f4
iderraz <iderraz@hotmail.com>
2021-05-03 12:05:56 +0200

    partie:2-exo7

--- src/js/main.js
+++ src/js/main.js
@@ -26,6 +26,23 @@
 
 
 
+    /* - # Exercice 2 JavaScript -> Array */ 
+
+    /* - ## Créer une variable qui reçois un array avec 3 éléments -> ex: coding 11, coding 12, coding 13*/ 
+
+    let coding2 = ["coding11", "coding22", "coding33"]
+
+    /* - ## Afficher les éléments de l'array*/
+
+    console.log(coding2);
+
+    /* - ## Ajouter et afficher votre prénom a l'array*/
+
+    coding2.push("GG-EZ");
+    console.log(coding2);
+
+
+
 
 
 
