55f37267efde9b4c677fbf3a138da04ee4d621c4
iderraz <iderraz@hotmail.com>
2021-05-03 11:46:34 +0200

    js-exo-7

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+/*
+# Exercice 1 JavaScript -> Array
+
+    - ## Créer une variable qui reçois un array avec 3 éléments -> ex: coding 11, coding 12, coding 13*/
+
+    let coding = ["coding 11", "coding 12", "coding 13"]
+
+
+    /* - ##  Afficher les éléments de l'array*/
+
+    console.log(coding);
+
+
+    /* - ## Afficher le nombre d'éléments de l'array*/
+
+    console.log(coding.length);
+
+
+    /* - ## Afficher les éléments de l'array 1 par 1 */
+
+    console.log(coding[0]);
+
+    console.log(coding[1]);
+
+    console.log(coding[2]);
+
+
+
+
+
+
+
