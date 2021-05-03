d287b42387f93ba7ba028d8ec5b8b3a5cfff7782
iderraz <iderraz@hotmail.com>
2021-05-03 12:16:27 +0200

    partie:3 exo-7

--- src/js/main.js
+++ src/js/main.js
@@ -26,7 +26,7 @@
 
 
 
-    /* - # Exercice 2 JavaScript -> Array */ 
+    /* - # EXERCICE 2 JavaScript -> Array */ 
 
     /* - ## Créer une variable qui reçois un array avec 3 éléments -> ex: coding 11, coding 12, coding 13*/ 
 
@@ -40,6 +40,25 @@
 
     coding2.push("GG-EZ");
     console.log(coding2);
+
+
+
+    /* - # Exercice 3 JavaScript -> Array */
+
+    /* - ## Créer une variable qui reçois un array avec 3 éléments -> ex: coding 11, coding 12, coding 13*/
+    let coding3 = ["coding11", "coding22", "coding33"]
+
+    /*- ## Afficher les éléments de l'array*/
+    console.log(coding3);
+
+    /* - ## Ajouter et afficher votre prenom a l'array*/
+    coding3.push("GG-EZ");
+    console.log(coding3);
+
+    /* - ## Remplacer et afficher un élément de l'array par autre chose*/
+
+    coding3.splice(1, 0, "what's up dude");
+    console.log(coding3);
 
 
 
